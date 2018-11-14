cec421bd5d80804a607a0e886b1d42c46d35eb0d
Mariusz Kowalewski <Mariusz.Kowalewski@eu.jll.com>
2018-11-14 12:51:18 +0100

    feat: Add ES6 classes notes

---
+++ es-clasess.js
@@ -0,0 +1,105 @@
+// ES6 Classes
+class Plane {
+  constructor(numEngines) {
+    this.numEngines = numEngines;
+    this.enginesActive = false;
+  }
+
+  static badWeather(planes) {
+    for (plane of planes) {
+      plane.enginesActive = false;
+    }
+  }
+
+  startEngines() {
+    console.log('starting engines…');
+    this.enginesActive = true;
+  }
+}
+
+const richardsPlane = new Plane(1);
+richardsPlane.startEngines();
+
+typeof Plane; // Returns: function
+
+// Call static methods
+Plane.badWeather([plane1, plane2, plane3]);
+
+
+// Subclasses with ES6 - super and extends
+
+class Tree {
+  constructor(size = '10', leaves = {spring: 'green', summer: 'green', fall: 'orange', winter: null}) {
+    this.size = size;
+    this.leaves = leaves;
+    this.leafColor = null;
+  }
+
+  changeSeason(season) {
+    this.leafColor = this.leaves[season];
+    if (season === 'spring') {
+      this.size += 1;
+    }
+  }
+}
+
+class Maple extends Tree {
+  constructor(syrupQty = 15, size, leaves) {
+    super(size, leaves);
+    this.syrupQty = syrupQty;
+  }
+
+  changeSeason(season) {
+    super.changeSeason(season);
+    if (season === 'spring') {
+      this.syrupQty += 1;
+    }
+  }
+
+  gatherSyrup() {
+    this.syrupQty -= 3;
+  }
+}
+
+const myMaple = new Maple(15, 5);
+myMaple.changeSeason('fall');
+myMaple.gatherSyrup();
+myMaple.changeSeason('spring');
+
+
+/*
+ * Programming Quiz: Building Classes and Subclasses (2-3)
+ * Create a Bicycle subclass that extends the Vehicle class. The Bicycle subclass should override Vehicle's constructor function by changing the default values for wheels from 4 to 2 and horn from 'beep beep' to 'honk honk'
+ */
+
+class Vehicle {
+	constructor(color = 'blue', wheels = 4, horn = 'beep beep') {
+		this.color = color;
+		this.wheels = wheels;
+		this.horn = horn;
+	}
+
+	honkHorn() {
+		console.log(this.horn);
+	}
+}
+
+class Bicycle extends Vehicle {
+    constructor(color, wheels = 2, horn = 'honk honk') {
+        super(color);
+        this.wheels = wheels;
+        this.horn = horn;
+    }
+}
+
+// your code goes here
+
+// tests
+const myVehicle = new Vehicle();
+myVehicle.honkHorn(); // beep beep
+const myBike = new Bicycle();
+myBike.honkHorn(); // honk honk
+
+// Prints:
+// beep beep
+// honk honk
