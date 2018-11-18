426c4258e53bd1a0196cbd48ae0cc28bbfe05838
Mario <kowalewski.mariusz@gmail.com>
2018-11-18 17:12:10 +0100

    feat: Add second part of es6 built-ins notes

---
+++ es6-built-ins2.js
@@ -0,0 +1,390 @@
+// Sets & Iterators
+const iterator = months.values();
+iterator.next(); // Object {value: 'January', done: false}
+iterator.next(); // Object {value: 'February', done: false}
+
+const colors = new Set(['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'brown', 'black']);
+for (const color of colors) {
+  console.log(color);
+}
+
+/* Prints:
+red
+orange
+yellow
+green
+blue
+violet
+brown
+black */
+
+/*
+ * Programming Quiz: Using Sets (3-1)
+ *
+ * Create a Set object and store it in a variable named `myFavoriteFlavors`. Add the following strings to the set:
+ *     - chocolate chip
+ *     - cookies and cream
+ *     - strawberry
+ *     - vanilla
+ *
+ * Then use the `.delete()` method to remove "strawberry" from the set.
+ */
+
+const myFavoriteFlavors = new Set();
+
+myFavoriteFlavors.add("chocolate chip");
+myFavoriteFlavors.add("cookies and cream");
+myFavoriteFlavors.add("strawberry");
+myFavoriteFlavors.add("vanilla");
+myFavoriteFlavors.delete("strawberry");
+
+/* WeakSets
+ *
+ * A WeakSet is just like a normal Set with a few key differences:
+ * - a WeakSet can only contain objects
+ * - a WeakSet is not iterable which means it can’t be looped over
+ * - a WeakSet does not have a .clear() method
+ */
+
+let student1 = { name: 'James', age: 26, gender: 'male' };
+let student2 = { name: 'Julia', age: 27, gender: 'female' };
+let student3 = { name: 'Richard', age: 31, gender: 'male' };
+
+const roster = new WeakSet([student1, student2, student3]);
+console.log(roster);
+
+// Prints: WeakSet {Object {name: 'Julia', age: 27, gender: 'female'}, Object {name: 'Richard', age: 31, gender: 'male'}, Object {name: 'James', age: 26, gender: 'male'}}
+
+student3 = null;
+console.log(roster);
+
+// Prints: WeakSet {Object {name: 'Julia', age: 27, gender: 'female'}, Object {name: 'James', age: 26, gender: 'male'}}
+
+/*
+ * Programming Quiz: Using Sets (3-2)
+ *
+ * Create the following variables:
+ *     - uniqueFlavors and set it to a new WeakSet object
+ *     - flavor1 and set it equal to `{ flavor: 'chocolate' }`
+ *     - flavor2 and set it equal to an object with property 'flavor' and value of your choice!
+ *
+ * Use the `.add()` method to add the objects `flavor1` and `flavor2` to `uniqueFlavors`
+ * Use the `.add()` method to add the `flavor1` object (again!) to the `uniqueFlavors` set
+ */
+
+const uniqueFlavors = new WeakSet();
+let flavor1 = { flavor: 'chocolate' };
+let flavor2 = { flavor: 'coffee' };
+
+uniqueFlavors.add(flavor1);
+uniqueFlavors.add(flavor2);
+uniqueFlavors.add(flavor1);
+
+/* Maps
+
+If Sets are similar to Arrays, then Maps are similar to Objects because Maps store key-value pairs similar to how objects contain named properties with values.
+
+Essentially, a Map is an object that lets you store key-value pairs where both the keys and the values can be objects, primitive values, or a combination of the two.*/
+
+const employees = new Map();
+console.log(employees); // Map {}
+
+employees.set('james.parkes@udacity.com', {
+    firstName: 'James',
+    lastName: 'Parkes',
+    role: 'Content Developer'
+});
+employees.set('julia@udacity.com', {
+    firstName: 'Julia',
+    lastName: 'Van Cleve',
+    role: 'Content Developer'
+});
+employees.set('richard@udacity.com', {
+    firstName: 'Richard',
+    lastName: 'Kalehoff',
+    role: 'Content Developer'
+});
+
+console.log(employees);
+
+// Prints: Map {'james.parkes@udacity.com' => Object {...}, 'julia@udacity.com' => Object {...}, 'richard@udacity.com' => Object {...}}
+
+employees.delete('julia@udacity.com');
+employees.delete('richard@udacity.com');
+console.log(employees);
+
+// Prints: Map {'james.parkes@udacity.com' => Object {firstName: 'James', lastName: 'Parkes', role: 'Course Developer'}}
+
+employees.clear()
+console.log(employees); // Map {}
+
+
+// Working with Maps
+const members = new Map();
+
+members.set('Evelyn', 75.68);
+members.set('Liam', 20.16);
+members.set('Sophia', 0);
+members.set('Marcus', 10.25);
+
+console.log(members.has('Xavier')); // fakse
+console.log(members.has('Marcus')); // true
+console.log(members.get('Evelyn')); // 75.68
+
+
+// Looping through Maps
+
+// 1. Using the MapIterator
+let iteratorObjForKeys = members.keys();
+iteratorObjForKeys.next(); // Object {value: 'Evelyn', done: false}
+iteratorObjForKeys.next(); // Object {value: 'Liam', done: false}
+
+let iteratorObjForValues = members.values();
+iteratorObjForValues.next(); // Object {value: 75.68, done: false}
+
+// 2. Using a for...of Loop
+for (const member of members) {
+  console.log(member);
+}
+
+/* ['Evelyn', 75.68]
+ ['Liam', 20.16]
+ ['Sophia', 0]
+ ['Marcus', 10.25] */
+
+ /*
+ * Using array destructuring, fix the following code to print the keys and values of the `members` Map to the console.
+ */
+
+const members = new Map();
+
+members.set('Evelyn', 75.68);
+members.set('Liam', 20.16);
+members.set('Sophia', 0);
+members.set('Marcus', 10.25);
+
+for (const member of members) {
+    let [key, value] = member;
+    console.log(key, value);
+}
+
+// 3. Using a forEach Loop
+members.forEach((value, key) => console.log(key, value));
+
+/* 'Evelyn' 75.68
+ 'Liam' 20.16
+ 'Sophia' 0
+ 'Marcus' 10.25 */
+
+
+// Promises
+new Promise(function () {
+    window.setTimeout(function createSundae(flavor = 'chocolate') {
+        const sundae = {};
+        // request ice cream
+        // get cone
+        // warm up ice cream scoop
+        // scoop generous portion into cone!
+        if ( /* iceCreamConeIsEmpty(flavor) */ ) {
+            reject(`Sorry, we're out of that flavor :-(`);
+        }
+        resolve(sundae);
+    }, Math.random() * 2000);
+});
+
+// This code creates a promise that will start in a few seconds after I make the request. Then there are a number of steps that need to be made in the createSundae function.
+
+// Promises Return Immediately
+const myPromiseObj = new Promise(function (resolve, reject) {
+    // sundae creation code
+});
+
+/* That object has a .then() method on it that we can use to have it notify us if the request we made in the promise was either successful or failed. The .then() method takes two functions:
+ *
+ * the function to run if the request completed successfully (resolve)
+ * the function to run if the request failed to complete (reject)
+ */
+
+mySundae.then(function(sundae) {
+    console.log(`Time to eat my delicious ${sundae}`);
+}, function(msg) {
+    console.log(msg);
+    self.goCry(); // not a real method
+});
+
+
+// Proxies
+var richard = {status: 'looking for work'};
+var agent = new Proxy(richard, {});
+
+agent.status; // returns 'looking for work'
+
+// Get Trap
+// The "get" trap is used to "intercept" calls to properties:
+
+const handler = {
+    get(target, propName) {
+        console.log(target); // the `richard` object, not `handler` and not `agent`
+        console.log(propName); // the name of the property the proxy (`agent` in this case) is checking
+    }
+};
+const agent = new Proxy(richard, handler);
+agent.status; // logs out the richard object (not the agent object!) and the name of the property being accessed (`status`)
+
+// Accessing the Target object from inside the proxy
+const richard = {status: 'looking for work'};
+const handler = {
+    get(target, propName) {
+        console.log(target);
+        console.log(propName);
+        return target[propName];
+    }
+};
+const agent = new Proxy(richard, handler);
+agent.status; // (1)logs the richard object, (2)logs the property being accessed, (3)returns the text in richard.status
+
+// Having the proxy return info, directly
+const richard = {status: 'looking for work'};
+const handler = {
+    get(target, propName) {
+        return `He's following many leads, so you should offer a contract as soon as possible!`;
+    }
+};
+const agent = new Proxy(richard, handler);
+agent.status; // returns the text `He's following many leads, so you should offer a contract as soon as possible!`
+
+// get trap will take over whenever any property on the proxy is accessed. If we want to intercept calls to change properties, then the set trap needs to be used!
+
+/* The set trap is used for intercepting code that will change a property. The set trap receives: the object it proxies the property that is being set the new value for the proxy */
+const richard = {status: 'looking for work'};
+const handler = {
+    set(target, propName, value) {
+        if (propName === 'payRate') { // if the pay is being set, take 15% as commission
+            value = value * 0.85;
+        }
+        target[propName] = value;
+    }
+};
+const agent = new Proxy(richard, handler);
+agent.payRate = 1000; // set the actor's pay to $1,000
+agent.payRate; // $850 the actor's actual pay
+
+
+// Generators
+function* getEmployee() {
+    console.log('the function has started');
+
+    const names = ['Amanda', 'Diego', 'Farrin', 'James', 'Kagure', 'Kavita', 'Orit', 'Richard'];
+
+    for (const name of names) {
+        console.log(name);
+        yield;
+    }
+
+    console.log('the function has ended');
+}
+
+getEmployee();
+
+// this is the response I get in Chrome:
+getEmployee {[[GeneratorStatus]]: "suspended", [[GeneratorReceiver]]: Window}
+
+const generatorIterator = getEmployee();
+generatorIterator.next();
+
+/* Prints:
+ * the function has started
+ * Amanda
+ */
+
+// Yielding Data to the "Outside" World
+function* getEmployee() {
+    console.log('the function has started');
+
+    const names = ['Amanda', 'Diego', 'Farrin', 'James', 'Kagure', 'Kavita', 'Orit', 'Richard'];
+
+    for (const name of names) {
+        yield name;
+    }
+
+    console.log('the function has ended');
+}
+
+const generatorIterator = getEmployee();
+let result = generatorIterator.next();
+result.value // is "Amanda"
+
+generatorIterator.next().value // is "Diego"
+generatorIterator.next().value // is "Farrin"
+
+// Sending data into/out of a Generator
+function* displayResponse() {
+    const response = yield;
+    console.log(`Your response is "${response}"!`);
+}
+
+const iterator = displayResponse();
+
+iterator.next(); // starts running the generator function
+iterator.next('Hello Udacity Student'); // send data into the generator
+// the line above logs to the console: Your response is "Hello Udacity Student"!
+
+function* getEmployee() {
+    const names = ['Amanda', 'Diego', 'Farrin', 'James', 'Kagure', 'Kavita', 'Orit', 'Richard'];
+    const facts = [];
+
+    for (const name of names) {
+        // yield *out* each name AND store the returned data into the facts array
+        facts.push(yield name);
+    }
+
+    return facts;
+}
+
+const generatorIterator = getEmployee();
+
+// get the first name out of the generator
+let name = generatorIterator.next().value;
+
+// pass data in *and* get the next name
+name = generatorIterator.next(`${name} is cool!`).value;
+
+// pass data in *and* get the next name
+name = generatorIterator.next(`${name} is awesome!`).value;
+
+// pass data in *and* get the next name
+name = generatorIterator.next(`${name} is stupendous!`).value;
+
+// you get the idea
+name = generatorIterator.next(`${name} is rad!`).value;
+name = generatorIterator.next(`${name} is impressive!`).value;
+name = generatorIterator.next(`${name} is stunning!`).value;
+name = generatorIterator.next(`${name} is awe-inspiring!`).value;
+
+// pass the last data in, generator ends and returns the array
+const positions = generatorIterator.next(`${name} is magnificent!`).value;
+
+// displays each name with description on its own line
+positions.join('\n');
+
+// QUIZ QUESTION
+// What will happen if the following code is run?
+
+function* createSundae() {
+    const toppings = [];
+
+    toppings.push(yield);
+    toppings.push(yield);
+    toppings.push(yield);
+
+    return toppings;
+}
+
+var it = createSundae();
+it.next('hot fudge');
+it.next('sprinkles');
+it.next('whipped cream');
+it.next();
+
+// Answer: The toppings array will have undefined as its last item
+// Because the first call to .next() passes in some data. But that data doesn't get stored anywhere. The last call to .next() should have some data since it's being yielded into the last call to toppings.push().
